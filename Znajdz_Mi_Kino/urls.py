9351fa114dc3f62d3b9c2b363b351f5ab48128db
Piotr Waśko <piiteerw@gmail.com>
2015-04-20 14:33:45 +0200

    Zmiana wyglądu linków, Wyświetlanie repertuaru wg filmu, kina i daty

--- Znajdz_Mi_Kino/urls.py
+++ Znajdz_Mi_Kino/urls.py
@@ -6,10 +6,15 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'Znajdz_Mi_Kino.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', index, name='index'),
     url(r'^map/$', mapa, name='mapa'),
-    url(r'^repertuar/(?P<c_type>[\w+%-]*)/(?P<what>[\w+%-]*)/$', repertuar, name='repertuar'),
+    url(r'^repertuar/kino/(?P<cinema>\d+)/0/$', by_cinema, name='by_cinema'),
+    url(r'^repertuar/kino/(?P<cinema>\d+)/(?P<day>[012])/$', by_cinema, name='by_cinema'),
+    url(r'^repertuar/film/(?P<movie>\d+)/0/$', by_movie, name='by_movie'),
+    url(r'^repertuar/film/(?P<movie>\d+)/(?P<day>[012])/$', by_movie, name='by_movie'),
+    url(r'^repertuar/0/$', repertuar, name='repertuar'),
+    url(r'^repertuar/(?P<day>[012])/$', repertuar, name='repertuar'),
     url(r'^import/$', data_import),
 
 )
--- app/views.py
+++ app/views.py
@@ -1,64 +1,83 @@
 from urllib import quote_plus, unquote_plus
-
+from datetime import timedelta
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.utils.timezone import is_aware
 from dbfun import *
 from app.tasks import *
 
 
 def index(request):
     all_cinemas = getCinemas()
-    for c in all_cinemas:
-        c['link_name'] = quote_plus(c['name'].encode('utf-8'))
-        c['link_type'] = quote_plus(c['cinema_type'].encode('utf-8'))
+    all_movies = getMovies()
 
     context = {
         'all_cinemas': all_cinemas,
+        'all_movies': all_movies,
     }
     return render(request, 'index.html', context)
 
+
 def mapa(request):
     all_cinemas = getCinemas()
-    for c in all_cinemas:
-        c['link_name'] = quote_plus(c['name'].encode('utf-8'))
-        c['link_type'] = quote_plus(c['cinema_type'].encode('utf-8'))
 
     context = {
         'all_cinemas': all_cinemas
     }
     return render(request, 'map.html', context)
 
-def repertuar(request, c_type, what):
-    all_cinemas = getCinemas()
-    for c in all_cinemas:
-        c['link_name'] = quote_plus(c['name'].encode('utf-8'))
-        c['link_type'] = quote_plus(c['cinema_type'].encode('utf-8'))
 
-    if c_type != all and what != 'all':
-        name = unquote_plus(what.encode('utf-8')).decode('utf-8')
-        cinema_type = unquote_plus(c_type.encode('utf-8')).decode('utf-8')
-        cin = getByCinema(cinema_type, name)
-        t_begin = ""
-        t_end = "2016"
-        shows = []
-        for mv in cin['movies']:
-            for time in mv['shows']:
-                if t_begin <= time and time <= t_end:
-                    shows.append({'date': time, 'title': mv['title'], 'name': name, 'address': cin['address']})
-    else:
-        name = "Wszystkie kina"
-        cinema_type = ""
-        shows = getShows()
-        #shows = {}
+def repertuar(request, day="0"):
+    all_cinemas = getCinemas()
+    d = date.today() + timedelta(int(day))
+    shows = getShows(d.year, d.month, d.day).order_by("date")
 
     context = {
-        'all_cinemas': all_cinemas,
-        'name': name,
-        'cinema_type': cinema_type,
         'shows': shows,
+        'name': "Repertuar wszystkich kin",
+        'all_cinemas': all_cinemas,
     }
     return render(request, 'repertuar.html', context)
+
+
+def by_cinema(request, cinema, day="0"):
+    all_cinemas = getCinemas()
+    if day == "0":
+        d1 = datetime.now(pytz.timezone("Europe/Warsaw")) + timedelta(int(day))
+    else:
+        d1 = datetime.today() + timedelta(int(day))
+    d2 = d1 + timedelta(1)
+
+    c = Cinema.objects.get(id=cinema)
+    shows = getByCinema(c.cinema_type, c.name).filter(date__range=(d1, d2)).order_by("date")
+
+    context = {
+        'shows': shows,
+        'name': "Repertuar dla kina " + c.cinema_type + " - " + c.name,
+        'all_cinemas': all_cinemas,
+    }
+    return render(request, 'repertuar.html', context)
+
+
+def by_movie(request, movie, day="0"):
+    all_cinemas = getCinemas()
+    if day == "0":
+        d1 = datetime.now(pytz.timezone("Europe/Warsaw")) + timedelta(int(day))
+    else:
+        d1 = datetime.today() + timedelta(int(day))
+    d2 = d1 + timedelta(1)
+    m = Movie.objects.get(id=movie)
+    print "Movies", d1, "-", d2
+    shows = getByMovie(m.title).filter(date__range=(d1, d2)).order_by("date")
+    for sh in shows:
+        print sh.date
+    context = {
+        'shows': shows,
+        'name': "Repertuar filmu \"" + m.title + "\"",
+        'all_cinemas': all_cinemas,
+    }
+    return render(request, 'repertuar.html', context)
+
 
 def data_import(request):
     importData()
